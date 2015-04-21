ec0d99fa627b9495a1b99ea02d405327b71a0cba
feiyu <yinfeiyu43@gmail.com>
2015-04-20 17:12:21 -0700

    modify models, implement and tested create method via POST method

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -2,12 +2,13 @@ package controllers;
 
 import com.avaje.ebean.Ebean;
 import com.fasterxml.jackson.databind.JsonNode;
+import models.Category;
 import models.User;
-import org.springframework.context.support.LiveBeansViewMBean;
 import play.db.ebean.Model;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static play.libs.Json.toJson;
@@ -22,15 +23,41 @@ public class UserController extends Controller {
         if(json == null){
             return badRequest("Expecting a Json input");
         }
+        if(json.findPath("firstName") == null){
+            return badRequest("No firstName field");
+        }
+        if(json.findPath("lastName") == null){
+            return badRequest("No lastName field");
+        }
+        if(json.findPath("email") == null){
+            return badRequest("No email field");
+        }
+        if(json.findPath("password") == null){
+            return badRequest("No password field");
+        }
+        if(json.findValuesAsText("cIds") == null){
+            return badRequest("No cIds field");
+        }
+
         String firstName = json.findPath("firstName").textValue();
         String lastName = json.findPath("lastName").textValue();
         String email = json.findPath("email").textValue();
         String password = json.findPath("password").textValue();
+        List<String> cNames =  json.findValuesAsText("cIds");
+
+        List<Category> cs = new ArrayList<Category>();
+        for(int i = 0; i < cNames.size(); i ++){
+            Category c = (Category) new Model.Finder(String.class, Category.class).byId(Long.parseLong(cNames.get(i)));
+            if (c != null){
+                cs.add(c);
+            }
+        }
         User u = new User();
         u.setEmail(email);
         u.setFirstName(firstName);
         u.setLastName(lastName);
         u.setPassword(password);
+        u.setExpertises(cs);
         Ebean.save(u);
         return ok(toJson(u));
     }
@@ -57,15 +84,32 @@ public class UserController extends Controller {
         if(json == null){
             return badRequest("Expecting a Json input");
         }
-        String firstName = json.findPath("firstName").textValue();
-        String lastName = json.findPath("lastName").textValue();
-        String email = json.findPath("email").textValue();
-        String password = json.findPath("password").textValue();
 
-        u.setEmail(email);
-        u.setFirstName(firstName);
-        u.setLastName(lastName);
-        u.setPassword(password);
+        List<String> cNames =  json.findValuesAsText("cIds");
+
+        List<Category> cs = new ArrayList<Category>();
+        for(int i = 0; i < cNames.size(); i ++){
+            Category c = (Category) new Model.Finder(String.class, Category.class).byId(Long.parseLong(cNames.get(i)));
+            if (c != null){
+                cs.add(c);
+            }
+        }
+
+        if(json.findPath("firstName") != null){
+            u.setFirstName(json.findPath("firstName").textValue());
+        }
+        if(json.findPath("lastName") != null){
+            u.setLastName(json.findPath("lastName").textValue());
+        }
+        if(json.findPath("email") != null){
+            u.setEmail(json.findPath("email").textValue());
+        }
+        if(json.findPath("password") != null){
+            u.setPassword(json.findPath("password").textValue());
+        }
+        if(cs.size() != 0){
+            u.setExpertises(cs);
+        }
         Ebean.save(u);
         return ok(toJson(u));
     }
